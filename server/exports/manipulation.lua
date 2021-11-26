9a99db66abc697b788c1fbe3dacb6e1514c9284c
Diary <buddy1875@hotmail.com>
2021-11-26 13:19:47 +0700

    xsound

---
+++ server/exports/manipulation.lua
@@ -0,0 +1,68 @@
+function Position(source, name_, pos)
+    TriggerClientEvent("xsounddj:stateSound", source, "position", {
+        soundId = name_,
+        position = pos,
+    })
+end
+
+exports('Position', Position)
+
+function Distance(source, name_, distance_)
+    TriggerClientEvent("xsounddj:stateSound", source, "distance", {
+        soundId = name_,
+        distance = distance_,
+    })
+end
+
+exports('Distance', Distance)
+
+function Destroy(source, name_)
+    TriggerClientEvent("xsounddj:stateSound", source, "destroy", {
+        soundId = name_,
+    })
+end
+
+exports('Destroy', Destroy)
+
+function Pause(source, name_)
+    TriggerClientEvent("xsounddj:stateSound", source, "pause", {
+        soundId = name_,
+    })
+end
+
+exports('Pause', Pause)
+
+function Resume(source, name_)
+    TriggerClientEvent("xsounddj:stateSound", source, "resume", {
+        soundId = name_,
+    })
+end
+
+exports('Resume', Resume)
+
+function setVolume(source, name_, vol)
+    TriggerClientEvent("xsounddj:stateSound", source, "volume", {
+        soundId = name_,
+        volume = vol,
+    })
+end
+
+exports('setVolume', setVolume)
+
+function setTimeStamp(source, name_, time_)
+    TriggerClientEvent("xsounddj:stateSound", source, "timestamp", {
+        soundId = name_,
+        time = time_
+    })
+end
+
+exports('setTimeStamp', setTimeStamp)
+
+function destroyOnFinish(id, bool)
+    TriggerClientEvent("xsounddj:stateSound", source, "destroyOnFinish", {
+        soundId = id,
+        value = bool
+    })
+end
+
+exports('destroyOnFinish', destroyOnFinish)
